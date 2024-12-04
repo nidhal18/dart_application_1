5a62e085f44b19cbe242abcc4ffd5901cfe143da
Nidhal <alinidhal31@gmail.com>
2024-12-04 13:45:07 +0530

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,42 @@
+void main() {
+  var fruits = ['orange', 'banana', 'watermelon', 'pineapple'];
+
+  //print(fruits.contains('mango'));
+
+  //fruits.forEach((fruit) => print(fruit));
+
+//var mappedFruits = fruits.map((fruit) => 'I like $fruit');
+//print(mappedFruits);
+
+  List<Map<String, dynamic>> persons = [
+    {'name': 'John', 'age': 16},
+    {'name': 'Peter', 'age': 40},
+    {'name': 'Jane', 'age': 31},
+    {'name': 'Mary', 'age': 23},
+    {'name': 'Ineza', 'age': 25},
+    {'name': 'Audrey', 'age': 63},
+  ];
+
+  //var result = persons.every((persons) => persons['age'] <= 60);
+  //print(result);
+
+  //var result = persons.where((persons) => persons['age'] <= 16);
+  // print(result);
+
+  //var result = persons.followedBy([
+  // {'name': 'Nadia', 'age': 28}
+  //]);
+  //print(result);
+
+  // persons.forEach((persons) => print(persons['age']));
+
+  // var result = persons.map<dynamic>((e) => e['age']).reduce((a, b) => a + b);
+  //print(result);
+
+  var numbers = [1, 3, 2, 5, 4];
+  // numbers.sort((num1, num2) => num1 - num2);
+  // print(numbers.reversed);
+
+  
+  print(value);
+}
